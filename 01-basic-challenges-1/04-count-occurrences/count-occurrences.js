c21f63238e490f80b2307d189c71f5a20ececc33
Andriy Trofimov <andriytrofimov@pop-os.localdomain>
2023-09-12 01:23:15 +0300

    Challenge-Count-Occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+// function countOccurrences(str, char) {
+// 	let count = 0;
+// 	for (let index = 0; index < str.length; index++) {
+// 		if (str[index] === char) {
+// 			count++;
+// 		}
+// 	}
+// 	return count;
+// }
 
+const countOccurrences = (str, char) => str.split(char).length - 1;
 module.exports = countOccurrences;
