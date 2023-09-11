bee2a27003d3efe8763c4bdef3406d0f401815cb
Andriy Trofimov <andriytrofimov@pop-os.localdomain>
2023-09-12 00:49:40 +0300

    Challenge-Calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,22 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+	let result;
+	switch (operator) {
+		case '+':
+			result = num1 + num2;
+			break;
+		case '-':
+			result = num1 - num2;
+			break;
+		case '*':
+			result = num1 * num2;
+			break;
+		case '/':
+			result = num1 / num2;
+			break;
+		default:
+			throw new Error('The operator you have passed is invalid');
+	}
+	return result;
+}
 
 module.exports = calculator;
