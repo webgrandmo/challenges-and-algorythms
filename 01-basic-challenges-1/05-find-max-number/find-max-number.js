15e8ccc8b83c3ebc9a65f4876ec1335f65d33264
Andriy Trofimov <webgrandmo@gmail.com>
2023-09-12 21:16:38 +0300

    Challenge-Find-max-number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+	let max = arr[0];
+
+	for (let index = 1; index < arr.length; index++) {
+		if (arr[index] > max) {
+			max = arr[index];
+		}
+	}
+	return max;
+}
 
 module.exports = findMaxNumber;
