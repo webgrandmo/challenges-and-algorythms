f5926e24357d7f679721a901a9e32ba555287977
Andriy Trofimov <webgrandmo@gmail.com>
2023-09-15 08:09:07 +0300

    Reverse string

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(string) {
+	const words = string.toLowerCase().split(' ');
+	for (let index = 0; index < words.length; index++) {
+		words[index] = words[index][0].toUpperCase() + words[index].slice(1);
+	}
+
+	return words.join(' ');
+}
 
 module.exports = titleCase;
