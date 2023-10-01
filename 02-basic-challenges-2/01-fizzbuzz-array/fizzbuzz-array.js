175eec7fc6af827f28d0bf8ea1088977cac3381e
Andriy Trofimov <webgrandmo@gmail.com>
2023-10-01 09:04:02 +0300

    FizzBuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,18 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(number) {
+	const array = [];
+	for (let i = 1; i <= number; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			array.push('FizzBuzz');
+		} else if (i % 5 === 0) {
+			array.push('Buzz');
+		} else if (i % 3 === 0) {
+			array.push('Fizz');
+		} else {
+			array.push(i);
+		}
+	}
+
+	return array;
+}
 
 module.exports = fizzBuzzArray;
