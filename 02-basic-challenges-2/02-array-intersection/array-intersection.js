a9f01a219f72d3b5b43182b5a620504c0dea9de4
Andriy Trofimov <webgrandmo@gmail.com>
2023-10-01 09:18:55 +0300

    intersection array

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,13 @@
-function arrayIntersection() {}
+function arrayIntersection(array1, array2) {
+	const intersectionArray = [];
+
+	for (let i = 0; i < array1.length; i++) {
+		if (array2.includes(array1[i]) && !intersectionArray.includes(array1[i])) {
+			intersectionArray.push(array1[i]);
+		}
+	}
+
+	return intersectionArray;
+}
 
 module.exports = arrayIntersection;
