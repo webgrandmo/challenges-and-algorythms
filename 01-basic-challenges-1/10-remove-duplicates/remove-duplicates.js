4a9d42f617001957f650dcee6a213844a1ed25e4
Andriy Trofimov <webgrandmo@gmail.com>
2023-10-01 08:33:11 +0300

    Remove duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,17 @@
-function removeDuplicates() {}
+// function removeDuplicates(array) {
+// 	const uniqueArray = [];
+
+// 	for (let i = 0; i < array.length; i++) {
+// 		if (!uniqueArray.includes(array[i])) {
+// 			uniqueArray.push(array[i]);
+// 		}
+// 	}
+
+// 	return uniqueArray;
+// }
+
+function removeDuplicates(array) {
+	return Array.from(new Set(array));
+}
 
 module.exports = removeDuplicates;
