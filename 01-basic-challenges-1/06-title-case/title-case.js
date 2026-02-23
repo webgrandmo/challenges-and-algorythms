f6f8cbf0d58ac2bce2710a734782842ea449858f
Andriy Trofimov <webgrandmo@gmail.com>
2026-02-23 19:03:39 +0200

    Title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,14 @@
-function titleCase() {}
+function titleCase(str) {
+	if (typeof str !== 'string') return NaN;
+	if (str.trim() === '') return;
+
+	const words = str.toLowerCase().split(' ');
+
+	for (let i = 0; i < words.length; i++) {
+		words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+	}
+
+	return words.join(' ');
+}
 
 module.exports = titleCase;
