89eca64c9163e96b2269eb8093b83011d51f414a
Andriy Trofimov <webgrandmo@gmail.com>
2026-02-25 19:57:02 +0200

    Count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,21 @@
-function countVowels() {}
+/**
+ * Returns the number of vowels in a string.
+ * @param {string} str - The string to search.
+ * @returns {number} - The number of vowels in the string.
+ */
+
+function countVowels(string) {
+	const formattedString = string.toLowerCase();
+	let count = 0;
+
+	for (let i = 0; i < formattedString.length; i++) {
+		const char = formattedString[i];
+		if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+			count++;
+		}
+	}
+
+	return count;
+}
 
 module.exports = countVowels;
