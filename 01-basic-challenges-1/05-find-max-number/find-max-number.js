32a3a67950a334d532423b9fa789948f61be0240
Andriy Trofimov <webgrandmo@gmail.com>
2026-02-28 18:19:42 +0200

    Max number

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,19 @@
-function findMaxNumber() {}
+/**
+ * Returns the largest number in an array.
+ * @param {number[]} arr - The array of numbers.
+ * @returns {number} - The largest number in the array.
+ */
+
+function findMaxNumber(numbers) {
+	let maxNum = numbers[0];
+
+	for (let i = 0; i < numbers.length; i++) {
+		if (typeof numbers[i] !== 'number') return NaN;
+		if (numbers[i] > maxNum) {
+			maxNum = numbers[i];
+		}
+	}
+	return maxNum;
+}
 
 module.exports = findMaxNumber;
