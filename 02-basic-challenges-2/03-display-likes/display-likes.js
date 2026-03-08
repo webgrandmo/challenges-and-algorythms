95ebbc91f71a698a2172fcab7f583d08a6ead395
Andriy Trofimov <webgrandmo@gmail.com>
2026-03-08 15:16:45 +0200

    Display likes challenge

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,19 @@
-function displayLikes() {}
+/**
+ * Returns a string of who likes the post.
+ * @param {string[]} names - The names of the people who like the post.
+ * @returns {string} - A string of who likes the post.
+ */
+
+function displayLikes(names) {
+	const count = names.length;
+	const templates = {
+		0: 'no one likes this',
+		1: `${names[0]} likes this`,
+		2: `${names[0]} and ${names[1]} like this`,
+		3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
+	};
+
+	return templates[count] || `${names[0]}, ${names[1]} and ${count - 2} others like this`;
+}
 
 module.exports = displayLikes;
