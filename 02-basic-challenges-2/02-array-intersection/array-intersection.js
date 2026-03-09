f8808d4a9aaa3fa3d3295aa58aac33129949924e
Andriy Trofimov <webgrandmo@gmail.com>
2026-03-09 18:17:54 +0200

    Array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,20 @@
-function arrayIntersection() {}
+/**
+ * Returns the intersection of two arrays.
+ * @param {number[]} arr1 - The first array.
+ * @param {number[]} arr2 - The second array.
+ * @returns {number[]} - The intersection of the two arrays.
+ */
+
+function arrayIntersection(arr1, arr2) {
+	const intersectedArray = [];
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr2.includes(arr1[i]) && !intersectedArray.includes(arr1[i])) {
+			intersectedArray.push(arr1[i]);
+		}
+	}
+
+	return intersectedArray;
+}
 
 module.exports = arrayIntersection;
