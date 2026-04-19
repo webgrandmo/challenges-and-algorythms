8981f8eafd32f3cb9e733368425125f9d8dbc605
Andriy Trofimov <webgrandmo@gmail.com>
2026-04-19 15:02:22 +0300

    are all chars unique

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,19 @@
-function areAllCharactersUnique() {}
+/**
+ * Returns true if all characters in a string are unique.
+ * @param {string} str - The string to check.
+ * @returns {boolean} - Whether all characters in the string are unique.
+ */
+
+// function areAllCharactersUnique(str) {
+// 	for (let i = 0; i < str.length; i++) {
+// 		if (str.indexOf(str[i]) !== str.lastIndexOf(str[i])) return false;
+// 	}
+// 	return true;
+// }
+
+function areAllCharactersUnique(str) {
+	const set = new Set(str);
+	return set.size === str.length;
+}
 
 module.exports = areAllCharactersUnique;
