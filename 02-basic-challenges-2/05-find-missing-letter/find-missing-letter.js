d55ad376f9bf04c729f252e74300890c406bc132
Andriy Trofimov <webgrandmo@gmail.com>
2026-04-19 11:46:28 +0300

    fond missing letters

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,22 @@
-function findMissingLetter() {}
+/**
+ * Returns the missing letter in an array of consecutive letters.
+ * @param {string[]} arr - An array of consecutive letters.
+ * @returns {string} - The missing letter.
+ */
+
+function findMissingLetter(arr) {
+	if (arr.length === 0) return '';
+
+	for (let i = 0; i < arr.length - 1; i++) {
+		const current = arr[i].charCodeAt(0);
+		const next = arr[i + 1].charCodeAt(0);
+
+		if (next !== current + 1) {
+			return String.fromCharCode(current + 1);
+		}
+	}
+
+	return '';
+}
 
 module.exports = findMissingLetter;
